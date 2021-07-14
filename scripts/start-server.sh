5e22f1c96a47966c5b9924cf03578950c24c1017
ich777 <christophhummer@gmail.com>
2021-07-14 10:53:32 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,50 +1,7 @@
 #!/bin/bash
 export DISPLAY=:0
 export XAUTHORITY=${DATA_DIR}/.Xauthority
-
-Xvfb :0 -screen scrn 800x600x16 2>/dev/null &
-
-LAT_V="$(wget -qO- https://github.com/ich777/versions/raw/master/luckyBackup | grep FORK | cut -d '=' -f2)"
-CUR_V="$(${DATA_DIR}/luckybackup --version 2> /dev/null | grep "version:" | cut -d ':' -f2 | xargs)"
-if [ -z $LAT_V ]; then
-	if [ -z $CUR_V ]; then
-		echo "---Can't get latest version of luckyBackup, putting container into sleep mode!---"
-		sleep infinity
-	else
-		echo "---Can't get latest version of luckyBackup, falling back to v$CUR_V---"
-		LAT_V="${CUR_V}"
-	fi
-fi
-
-kill -SIGTERM $(pidof Xvfb)
-
-echo "---Version Check---"
-if [ -z "$CUR_V" ]; then
-	echo "---luckyBackup not found, downloading and installing v$LAT_V...---"
-	cd ${DATA_DIR}
-	if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/luckyBackup-v$LAT_V.tar.gz "https://github.com/ich777/luckyBackup/releases/download/$LAT_V/luckyBackup-v${LAT_V}.tar.gz" ; then
-		echo "---Successfully downloaded luckyBackup v$LAT_V---"
-	else
-		echo "---Something went wrong, can't download luckyBackup v$LAT_V, putting container into sleep mode!---"
-		sleep infinity
-	fi
-	tar -C / --overwrite -xf ${DATA_DIR}/luckyBackup-v$LAT_V.tar.gz 2> /dev/null
-	rm ${DATA_DIR}/luckyBackup-v$LAT_V.tar.gz
-elif [ "$CUR_V" != "$LAT_V" ]; then
-	echo "---Version missmatch, installed v$CUR_V, downloading and installing latest v$LAT_V...---"
-	cd ${DATA_DIR}
-	rm -R ${DATA_DIR}/luckybackup*
-	if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/luckyBackup-v$LAT_V.tar.gz "https://github.com/ich777/luckyBackup/releases/download/$LAT_V/luckyBackup-v${LAT_V}.tar.gz" ; then
-		echo "---Successfully downloaded luckyBackup v$LAT_V---"
-	else
-		echo "---Something went wrong, can't download luckyBackup v$LAT_V, putting container into sleep mode!---"
-		sleep infinity
-	fi
-	tar -C / --overwrite -xf ${DATA_DIR}/luckyBackup-v$LAT_V.tar.gz 2> /dev/null
-	rm ${DATA_DIR}/luckyBackup-v$LAT_V.tar.gz
-elif [ "$CUR_V" == "$LAT_V" ]; then
-	echo "---luckyBackup v$CUR_V up-to-date---"
-fi
+export XDG_RUNTIME_DIR=/tmp/runtime-luckybackup
 
 echo "---Preparing Server---"
 if [ ! -d ${DATA_DIR}/.ssh ]; then
@@ -68,8 +25,6 @@ if [ ! -f ${DATA_DIR}/.ssh/ssh_host_ed25519_key ]; then
 else
     echo "---ssh_host_ed25519_key found!---"
 fi
-sed -i "/LuckyBackupDir=/c\LuckyBackupDir=${DATA_DIR}/.luckyBackup" ${DATA_DIR}/.luckyBackup/profiles/*.profile 2>/dev/null
-sed -i "/Default_Profile=/c\Default_Profile=${DATA_DIR}/.luckyBackup/profiles/default.profile" ${DATA_DIR}/.luckyBackup/settings.ini 2>/dev/null
 echo "---Starting ssh daemon---"
 /usr/sbin/sshd
 sleep 2
@@ -101,8 +56,16 @@ fi
 screen -wipe 2&>/dev/null
 chmod 700 ${DATA_DIR}/.ssh
 chmod 600 ${DATA_DIR}/.ssh/*
-chown ${UID}:crontab ${DATA_DIR}/.config/crontabs/*
-chmod 600 ${DATA_DIR}/.config/crontabs/*
+if [ ! -d ${DATA_DIR}/.cron ]; then
+	mkdir -p ${DATA_DIR}/.cron
+fi
+if [ ! "$(crontab -l 2>/dev/null)" ]; then
+  if [ ! -f ${DATA_DIR}/.luckyBackup/schedule/luckyCron.txt ]; then
+    crontab /tmp/cron
+  else
+    crontab ${DATA_DIR}/.luckyBackup/schedule/luckyCron.txt
+  fi
+fi
 
 echo "---Starting TurboVNC server---"
 vncserver -geometry ${CUSTOM_RES_W}x${CUSTOM_RES_H} -depth ${CUSTOM_DEPTH} :0 -rfbport ${RFB_PORT} -noxstartup ${TURBOVNC_PARAMS} 2>/dev/null
@@ -119,4 +82,4 @@ sleep 2
 
 echo "---Starting luckyBackup---"
 cd ${DATA_DIR}
-${DATA_DIR}/luckybackup
+/usr/bin/luckybackup
